bdc9f49908d904bff058d5f9cb1f1dad60d60f9e
ekgml4573 <67455705+ekgml4573@users.noreply.github.com>
2021-04-04 01:45:34 +0900

    날짜 세팅, 요일 매칭

--- app/src/main/java/com/example/android_project1/MonthActivity.java
+++ app/src/main/java/com/example/android_project1/MonthActivity.java
@@ -2,13 +2,88 @@ package com.example.android_project1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class MonthActivity extends AppCompatActivity {
+    /**
+     * 연/월 텍스트뷰
+     */
+    private TextView tvDate;
+    /**
+     * 그리드뷰 어댑터
+     */
+    private GridAdapter gridAdapter;
+
+    /**
+     * 일 저장 할 리스트
+     */
+    private ArrayList<String> dayList;
+
+    /**
+     * 그리드뷰
+     */
+    private GridView gridView;
+
+    /**
+     * 캘린더 변수
+     */
+    private Calendar mCal;
+
+    int year;
+    int month;
+    int day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent =getIntent();
+        year = intent.getIntExtra("year",-1);
+        month = intent.getIntExtra("month",-1);
+
+        if(year == -1 || month == -1){
+            year= Calendar.getInstance().get(Calendar.YEAR);
+            month= Calendar.getInstance().get(Calendar.MONTH);
+            day =Calendar.getInstance().get(Calendar.DATE);
+        }
+
+        tvDate = (TextView) findViewById(R.id.tv_date);
+        gridView = (GridView) findViewById(R.id.gridview);
+
+        // 오늘 날짜를 세팅 해준다.
+        long now = System.currentTimeMillis();
+        final Date date = new Date(now);
+        //연,월,일을 따로 저장
+        final SimpleDateFormat curYearFormat = new SimpleDateFormat("yyyy", Locale.KOREA);
+        final SimpleDateFormat curMonthFormat = new SimpleDateFormat("MM", Locale.KOREA);
+        final SimpleDateFormat curDayFormat = new SimpleDateFormat("dd", Locale.KOREA);
+
+        //gridview 요일 표시
+        dayList = new ArrayList<String>();
+
+        mCal = Calendar.getInstance();
+
+        TextView yearmonth = findViewById(R.id.yearmonth);
+        yearmonth.setText(year+"년 "+ (month+1) + "월");
+
+        //이번달 1일 무슨요일인지 판단 mCal.set(Year,Month,Day)
+        mCal.set(Integer.parseInt(curYearFormat.format(date)), Integer.parseInt(curMonthFormat.format(date)) - 1, 1);
+        int dayNum = mCal.get(Calendar.DAY_OF_WEEK);
+        //1일 - 요일 매칭 시키기 위해 공백 add
+        for (int i = 1; i < dayNum; i++) {
+            dayList.add("");
+        }
+        setCalendarDate(mCal.get(Calendar.MONTH) + 1);
+
     }
 }
