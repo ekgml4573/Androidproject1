52566af925792396eea22daf3f3a035a0fc7e6da
ekgml4573 <67455705+ekgml4573@users.noreply.github.com>
2021-04-30 03:26:19 +0900

    프래그먼트 관련

---
+++ app/src/main/java/com/example/android_project1/MonthCalendarAdapter.java
@@ -0,0 +1,27 @@
+package com.example.android_project1;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class MonthCalendarAdapter extends FragmentStateAdapter {
+    private static int NUM_ITEMS=100;
+
+    public MonthCalendarAdapter(@NonNull Fragment fragment) {
+        super(fragment);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+    int year = position;  //수정 필요
+    int month = position+1;  //수정 필요
+
+    return MonthCalendarFragment.newInstance(position, position+1);
+    }
+
+    @Override
+    public int getItemCount() {
+        return NUM_ITEMS;
+    }
+}
